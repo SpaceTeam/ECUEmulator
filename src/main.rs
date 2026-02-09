630a5b8ea7f682d6d17368f0a96ba6cd0bd8fcfc
fweichselbaum <fabian.weichselbaum@gmx.at>
2026-02-09 21:08:40 +0100

    use new naming for `CanMessageFrame` and modules

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod message_handling;
 mod protocol;
 
 use crate::message_handling::{handle_message, parse_can_message};
-use crate::protocol::message::send_message;
+use crate::protocol::message_conversion::send_message;
 use clap::Parser;
 use socketcan::{CanFdSocket, Socket};
 use std::env;
